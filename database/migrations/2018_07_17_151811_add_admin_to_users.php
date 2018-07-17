2c909526f5f0f0f9a09840fc8089f8ed5f838d23
Lasha Palelashvili <falelashvili@gmail.com>
2018-07-17 16:24:09 +0400

    Creating users profile

--- database/migrations/2018_07_17_151811_add_admin_to_users.php
+++ database/migrations/2018_07_17_151811_add_admin_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class AddAdminToUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -14,12 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->boolean('admin')->default(0)->after('email');
         });
     }
 
@@ -30,6 +25,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,9 +12,18 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         App\User::create([
-           'name' => 'Lasha Palelashvili',
+            'name' => 'Lasha Palelashvili',
             'email' => 'falelashvili@gmail.com',
-            'password' => bcrypt('123456')
+            'password' => bcrypt('123456'),
+            'admin' => 1
+        ]);
+
+        App\Profile::create([
+            'user_id' => 3,
+            'avatar' => 'link to image',
+            'about' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque, sed, tempora! Assumenda cum dolorem doloremque eligendi, eum facere fugiat hic libero, minima nihil placeat quas sequi sit veniam, voluptate voluptatum!',
+            'facebook' => 'facebook.com',
+            'youtube' => 'youtube.com'
         ]);
     }
 }
